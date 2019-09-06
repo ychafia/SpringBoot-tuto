fdb6f6833f5c43f015ebbcc0bb5d2d1ca8055d24
ychafia <chafiayouness@gmail.com>
2019-09-06 15:04:01 +0200

    init SpringBoot project - Microservices project MSA

---
+++ src/main/java/fr/youness/MSAProject/MsaProjectApplication.java
@@ -0,0 +1,13 @@
+package fr.youness.MSAProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaProjectApplication.class, args);
+	}
+
+}
